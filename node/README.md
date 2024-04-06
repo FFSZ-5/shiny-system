c5bd576830622403369ee2080507344532264a53
ffsz-5 <651828515@qq.com>
2024-04-06 19:57:26 +0800

    feat: node服务搭建

--- node/README.md
+++ node/README.md
@@ -3,3 +3,21 @@
 MySQL Server:提供数据存储和服务
 MySQL workbench：可视化的MySQL管理工具，用来操作存在MySQL里的数据
 # 二、SQL
+# 三、node搭建服务端
+## 1.依赖
+```js
+npm i express@4.17.1
+```
+## 2.跨域
+```JS
+npm i cors@2.8.5
+```
+```JS
+//跨域配置
+const cors=require('cors')
+app.use(cors())
+```
+## 3.解析表单数据
+```JS
+app.use(express.urlencoded({extended:false}))
+```
---
+++ node/app.js
@@ -0,0 +1,21 @@
+/**
+ * @Author: ffsz-5 651828515@qq.com
+ * @Date: 2024-04-01 19:33:36
+ * @LastEditors: ffsz-5 651828515@qq.com
+ * @LastEditTime: 2024-04-06 19:56:59
+ * @FilePath: \shiny-system\node\app.js
+ * @Description: 
+ **/
+const express=require('express')
+const app=express()
+const userRouter=require('./router/user')
+//跨域配置
+const cors=require('cors')
+app.use(cors())
+//解析表单数据:application/x-www-form-urlencoded
+app.use(express.urlencoded({extended:false}))
+app.use('/api',userRouter)
+app.listen(3007,()=>{
+console.log('lfsz','start 3007');
+
+})
