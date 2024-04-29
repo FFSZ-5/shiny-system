688b36d33eef95cacb13a35c61f2b4279f87766e
刘范思哲 <651828515@qq.com>
2024-04-29 12:21:15 +0800

    feat: 同步

--- node/router/user.js
+++ node/router/user.js
@@ -1,6 +1,14 @@
+/**
+ * @Author: liuyue.lfsz liufansizhe@come-future.com
+ * @Date: 2024-04-07 10:11:03
+ * @LastEditors: liuyue.lfsz liufansizhe@come-future.com
+ * @LastEditTime: 2024-04-07 15:03:19
+ * @FilePath: /shiny-system/node/router/user.js
+ * @Description: 
+ **/
 const express=require('express')
 const router=express.Router()
 const router_handle=require('../router_handle/user')
 router.post('/reguser',router_handle.regUser)
-router.post('/login',router_handle.login)
+router.get('/login',router_handle.login)
 module.exports=router
